070d2d96f4b4dd02fc24722708c53ff39b0f4604
Tyler Wymer <twymer@gmail.com>
2014-12-18 16:38:07 -0500

    Add geo location option

--- models/checkin.rb
+++ models/checkin.rb
@@ -11,21 +11,28 @@ class Checkin
   before :save, :set_geo_name
 
   def set_geo_name
-    params = {
-      username: ENV['GEO_USERNAME'],
-      password: ENV['GEO_PASSWORD'],
-      type: 'json',
-      maxRows: '1',
-      q: message
-    }
+    if (self.message =~ /^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$/).nil?
+      # if message is not coordinates, we need to look those up from geo data
+      params = {
+        username: ENV['GEO_USERNAME'],
+        password: ENV['GEO_PASSWORD'],
+        type: 'json',
+        maxRows: '1',
+        q: message
+      }
 
-    response = HTTParty.get("http://api.geonames.org/searchJSON", query: params)
-    json = JSON.parse(response.body)
-    location = json['geonames'].first
+      response = HTTParty.get("http://api.geonames.org/searchJSON", query: params)
+      json = JSON.parse(response.body)
+      location = json['geonames'].first
 
-    if location
-      self.lat = location['lat']
-      self.lng = location['lng']
+      if location
+        self.lat = location['lat']
+        self.lng = location['lng']
+      end
+    else
+      # if message is coordinates, then we just save those and move along
+      self.lat = message.split(',')[0].strip
+      self.lng = message.split(',')[1].strip
     end
   end
 end
--- public/js/app.js
+++ public/js/app.js
@@ -23,4 +23,21 @@ $(function () {
       }
     });
   });
+
+  $('#location-button').click(function (e) {
+    e.preventDefault();
+
+    function updateFromGeoLocation(position) {
+      coords = position.coords.latitude.toString();
+      coords += ', ';
+      coords += position.coords.longitude.toString();
+      $('#location-input').val(coords);
+    }
+
+    if (Modernizr.geolocation) {
+      navigator.geolocation.getCurrentPosition(updateFromGeoLocation);
+    } else {
+      alert("Aw, bummer, your browser isn't new enough to get geo location or you have it disabled or I don't know something broke, okay? You can just type it in instead.");
+    }
+  });
 });
